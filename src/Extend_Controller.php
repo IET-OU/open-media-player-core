5362ed37975a8688a6e48c440121b15220a64dd4
Nick Freear <nfreear@yahoo.co.uk>
2015-07-13 16:06:26 +0100

    Bug #7, Adding 'Extend_Controller' abstract class [iet:3748508]

---
+++ src/Extend_Controller.php
@@ -0,0 +1,23 @@
+<?php namespace IET_OU\Open_Media_Player;
+
+/**
+ * Extend this class to create a custom controller, by default at `/extend/{example}/1/2`
+ *
+ * @copyright Copyright 2015 The Open University.
+ */
+
+use \IET_OU\SubClasses\PluginInterface;
+
+abstract class Extend_Controller implements PluginInterface
+{
+    protected $method = 'xxyxzz';
+
+    abstract public function call($method, $params = array());
+
+    /** Called by SubClasses.
+    */
+    public function registerPlugin(array & $class_array)
+    {
+        $class_array[ $this->method ] = get_class($this);
+    }
+}
